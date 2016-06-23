b6246b737b64d80494267f6e7a8a227813ac7da6
bmichalik <bartosz.michalik@amartus.com>
2016-06-23 16:42:25 +0200

    refactoring on Main example

--- swagger-generator/src/main/java/com/mrv/yangtools/example/Main.java
+++ swagger-generator/src/main/java/com/mrv/yangtools/example/Main.java
@@ -1,8 +1,10 @@
 package com.mrv.yangtools.example;
 
-import com.amartus.yangtools.codegen.SwaggerGenerator;
+import com.mrv.yangtools.codegen.SwaggerGenerator;
+import com.mrv.yangtools.common.SchemaBuilder;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
 import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
 import org.opendaylight.yangtools.yang.parser.util.NamedFileInputStream;
@@ -20,10 +22,8 @@ import java.util.stream.Collectors;
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    static final String META_INF_YANG_STRING = "META-INF" + File.separator + "yang";
-    private static Path dir = Paths.get("/media/sf_transfer/yang");
-    public static void main(String[] args) throws IOException {
-        final SchemaContext ctx = parse(dir);
+    public static void main(String[] args) throws IOException, ReactorException {
+        final SchemaContext ctx = new SchemaBuilder().add(Paths.get(args[0])).build();
         log.info("Context parsed {}", ctx);
 
         final List<String> modules = Arrays.asList("mef-services", "mef-interfaces");
@@ -34,79 +34,5 @@ public class Main {
 
         generator.generate(new FileWriter("example.yaml"));
 
-    }
-
-    private static SchemaContext parse(Path dir) {
-        final CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
-        SchemaContext resolveSchemaContext;
-        List<Closeable> closeables = new ArrayList<>();
-        log.info("Inspecting {}", dir);
-        try {
-            /*
-             * Collect all files which affect YANG context. This includes all
-             * files in current project and optionally any jars/files in the
-             * dependencies.
-             */
-            final Collection<File> yangFilesInProject = getYangFiles(dir);
-
-            final List<NamedFileInputStream> yangsInProject = new ArrayList<>();
-            for (final File f : yangFilesInProject) {
-                // FIXME: This is hack - normal path should be reported.
-                yangsInProject.add(new NamedFileInputStream(f, META_INF_YANG_STRING + File.separator + f.getName()));
-            }
-
-            List<InputStream> all = new ArrayList<>();
-            all.addAll(yangsInProject);
-            closeables.addAll(yangsInProject);
-
-            /**
-             * Set contains all modules generated from input sources. Number of
-             * modules may differ from number of sources due to submodules
-             * (parsed submodule's data are added to its parent module). Set
-             * cannot contains null values.
-             */
-            Set<Module> projectYangModules;
-            try {
-                resolveSchemaContext = reactor.buildEffective(all);
-
-                Set<Module> parsedAllYangModules = resolveSchemaContext.getModules();
-                projectYangModules = new HashSet<>();
-                for (Module module : parsedAllYangModules) {
-                    final String path = module.getModuleSourcePath();
-                    if (path != null) {
-                        log.debug("Looking for source {}", path);
-                        for (NamedFileInputStream is : yangsInProject) {
-                            log.debug("In project destination {}", is.getFileDestination());
-                            if (path.equals(is.getFileDestination())) {
-                                log.debug("Module {} belongs to current project", module);
-                                projectYangModules.add(module);
-                                break;
-                            }
-                        }
-                    }
-                }
-            } finally {
-                for (AutoCloseable closeable : closeables) {
-                    closeable.close();
-                }
-            }
-
-            log.info("yang files parsed from {}", yangsInProject);
-            return resolveSchemaContext;
-
-            // MojoExecutionException is thrown since execution cannot continue
-        } catch (Exception e) {
-            log.error("Unable to parse yang files from {}", dir, e);
-
-            throw new IllegalArgumentException(" Unable to parse ynag files from " +
-                    dir, e);
-        }
-    }
-
-    private static Collection<File> getYangFiles(Path dir) throws IOException {
-        final PathMatcher yang = FileSystems.getDefault().getPathMatcher("glob:*.yang");
-        return Files.walk(dir)
-                .filter(p -> Files.isRegularFile(p) && yang.matches(p.getFileName()))
-                .map(Path::toFile).collect(Collectors.toList());
     }
 }
