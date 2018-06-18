7fd6ae00d31dd998851787ece3f0129ca7af519f
DR695H <dr695h@att.com>
2018-06-18 10:06:15 -0400

    [SDNGC-998] "fixing an issue where input or output of rpc is removed""
    This reverts commit 16b4d9bab9b671494e66b25e0d44f9e8dcc51af0.

--- swagger-generator/src/main/java/com/mrv/yangtools/codegen/impl/OptimizingDataObjectBuilder.java
+++ swagger-generator/src/main/java/com/mrv/yangtools/codegen/impl/OptimizingDataObjectBuilder.java
@@ -20,6 +20,8 @@ import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.*;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.GroupingEffectiveStatementImpl;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.InputEffectiveStatementImpl;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.OutputEffectiveStatementImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -127,9 +129,12 @@ public class OptimizingDataObjectBuilder extends AbstractDataObjectBuilder {
         }
 
         if(isAugmented.test(node)) return false;
+        if(node instanceof InputEffectiveStatementImpl || node instanceof OutputEffectiveStatementImpl) return false;
 
         Set<UsesNode> uses = uses(node);
-        return uses.size() == 1 && node.getChildNodes().stream().filter(n -> !n.isAddedByUses()).count() == 0;
+        boolean directGrouping = uses.size() == 1 && node.getChildNodes().stream().filter(n -> !n.isAddedByUses()).count() == 0;
+    	log.debug("isDirectGrouping for node: {} is: {}", node.toString(), directGrouping);
+        return directGrouping;
     }
 
     @Override
